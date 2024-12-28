acb5f28c716435cba1f8875b7ffc5297cd8cef83
matocdo2018 <lynguyenthanhtuan@gmail.com>
2024-12-28 22:36:06 +0700

    ok done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< Updated upstream
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    // TypeORM Module
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true, // Tự động load các entity
-        synchronize: true, // Chỉ dùng cho môi trường phát triển
-      }),
-    }),
-=======
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/database.config';
 
 @Module({
   imports: [
@@ -43,7 +22,6 @@ import { ThrottlerModule } from '@nestjs/throttler';
     TypeOrmModule.forRoot(databaseConfig),
     AuthModule,
     UsersModule,
->>>>>>> Stashed changes
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/config/database.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true, // Chỉ nên bật khi phát triển
+};
